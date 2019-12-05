a6e0048c70bc4a720a690f8b74194f03d585d139
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2019-12-05 19:55:56 +0100

    Add sign in wih email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './sign-in.styles.scss';
-import FormInput from '../form-input/form-input.component';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils.js';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import FormInput from '../form-input/form-input.component';
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -13,8 +13,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '', password: ''})
+    } catch (error) {
+      console.log(error)
+    }
     this.setState({ email: '', password: '' });
   };
 
@@ -28,7 +36,7 @@ class SignIn extends React.Component {
       <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
-        <form onSubmit={ this.handleSubmit }>
+        <form onSubmit={ this.handleSubmit } noValidate>
           <FormInput
             name="email"
             type="email"
