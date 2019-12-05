ad1ae3508067b5b2190455f56bcfdd210bb8127e
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2019-12-05 18:32:00 +0100

    Add Sign out logic

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,9 +45,10 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             value={this.state.password}
           />
-
-          <CustomButton type='submit'> Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign in with Google </CustomButton>
+          <div className="buttons">
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google </CustomButton>
+          </div>
         </form>
       </div>
     );
