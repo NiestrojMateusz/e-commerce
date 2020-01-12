6167601a0a65d1f4b985db5f9ac035fc1d5da22b
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2020-01-12 15:15:43 +0100

    Export collection overview to own component

---
+++ src/components/collection-overview/collection-overview.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import './collection-overview.styles.scss';
+import CollectionPreview from '../preview-collection/collection-preview.component';
+import { selectShopCollections } from '../../redux/shop/shop.selector';
+
+
+const CollectionsOverview = ({collections}) => (
+  <div className="collection-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+  </div>
+);
+
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections
+})
+
+
+export default connect(
+  mapStateToProps,
+)(CollectionsOverview);
